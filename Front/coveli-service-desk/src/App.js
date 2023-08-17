c8c25c62bbfc7c5d9e8185d00847a3ffea90da69
JesusGH123 <a01423730@itesm.mx>
2023-08-17 16:29:57 -0600

    More changes

--- Front/coveli-service-desk/src/App.js
+++ Front/coveli-service-desk/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
-import Login from './Login';
-import Home from './Home';
+import Login from './Login/Login';
+import Home from './Home/Home';
 import ProtectedRoutes from './ProtectedRoutes';
 
 function App() {
